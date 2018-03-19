28a7813e290b31973ff55e80cbbd4d142ccd93b5
DeimosA <krvolden@gmail.com>
2018-03-19 05:22:29 +0100

    Clear filter button

--- frontend/src/app/product-filter/product-filter.component.ts
+++ frontend/src/app/product-filter/product-filter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -15,8 +15,8 @@ export class ProductFilterComponent implements OnInit, OnDestroy {
   private formGroup : FormGroup = new FormGroup({
     search: new FormControl(''),
     sort: new FormControl('name ASC'),
-    minPrice: new FormControl(0),
-    maxPrice: new FormControl(Infinity)
+    minPrice: new FormControl(0, Validators.min(0)),
+    maxPrice: new FormControl(Infinity, Validators.min(0))
   });
 
   private formSubscription : Subscription;
@@ -34,6 +34,15 @@ export class ProductFilterComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(){
     this.formSubscription.unsubscribe();
+  }
+
+  private clearFilter() {
+    this.formGroup.reset({
+      search: '',
+      sort: 'name ASC',
+      minPrice: 0,
+      maxPrice: Infinity
+    });
   }
 
 }
